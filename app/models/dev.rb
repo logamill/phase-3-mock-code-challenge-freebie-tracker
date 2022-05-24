c836f4659b862e864c0e9b4994e234178815daf6
Logan Miller <loganmiller214@gmail.com>
2022-05-24 11:29:27 -0400

    finished challenge

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,19 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        if self.freebies.find_by item_name: item_name
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            Freebie.where(id: freebie.id).update(dev_id: dev.id)
+        end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -17,5 +22,22 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+# company_id = rand(1..4)
+# dev_id = rand(1..4)
+
+# name = Faker::Vehicle.make 
+# value = rand(5..50)
+
+# 20.times do 
+
+Freebie.create(item_name: "Pen", value: 5, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Dog", value: 1000, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "T-shirt", value: 15, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Train", value: 500, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Ice cream", value: 3, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Laptop", value: 2000, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Drone", value: 400, company_id: rand(1..4), dev_id: rand(1..4))
+
 
 puts "Seeding done!"
